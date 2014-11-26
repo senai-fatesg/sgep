0997b7e19ff438b91a1858ab793f4ed46591c286
rodrigo <rodrigo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2014-11-26 19:45:15 +0000

    Feito o autocomplete da sessao na página itensProva e feita a implementação dos seus métodos.

--- src/main/java/br/com/ambientinformatica/fatesg/sgep/persistencia/SessaoDaoJpa.java
+++ src/main/java/br/com/ambientinformatica/fatesg/sgep/persistencia/SessaoDaoJpa.java
@@ -1,5 +1,12 @@
 package br.com.ambientinformatica.fatesg.sgep.persistencia;
 
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import br.com.ambientinformatica.fatesg.sgep.entidade.Sessao;
@@ -9,5 +16,15 @@ import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
 public class SessaoDaoJpa extends PersistenciaJpa<Sessao> implements SessaoDao {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Override
+	public List<Sessao> consultarPeloTitulo(String titulo){
+		Session session = this.em.unwrap(Session.class);
+		Criteria criteria = session.createCriteria(Sessao.class);
+		
+		if (StringUtils.isNotBlank(titulo)) {
+			criteria.add(Restrictions.ilike("titulo", titulo.toUpperCase(), MatchMode.START));
+		}
+		return criteria.list();
+	}
 }
