205731ce706d5083ff20b9a27291ccc6f0abf45e
arthurbastos <arthurbastos@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-09 01:00:39 +0000

    Mudança dos padrões das telas, web.xml e na parte de cadastro de provas e sessão

--- trunk/sgep/src/main/java/br/com/ambientinformatica/fatesg/sgep/persistencia/QuestaoDaoJpa.java
+++ trunk/sgep/src/main/java/br/com/ambientinformatica/fatesg/sgep/persistencia/QuestaoDaoJpa.java
@@ -9,5 +9,7 @@ import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
 public class QuestaoDaoJpa extends PersistenciaJpa<Questao> implements QuestaoDao {
 
 	private static final long serialVersionUID = 1L;
+	
+	
 
 }
