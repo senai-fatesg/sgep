005b7a030af21a68a34d258329a5c7197937300f
gea.raphael <gea.raphael@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-11-24 02:46:58 +0000

    Camadas de percistencia.

--- src/main/java/br/com/ambientinformatica/fatesg/sgep/persistencia/QuestaoTemplateDaoJpa.java
+++ src/main/java/br/com/ambientinformatica/fatesg/sgep/persistencia/QuestaoTemplateDaoJpa.java
@@ -1,15 +1,62 @@
 package br.com.ambientinformatica.fatesg.sgep.persistencia;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.ambientinformatica.fatesg.sgep.entidade.QuestaoTemplate;
 import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
 
 @Repository("questaoTemplateDao")
-public class QuestaoTemplateDaoJpa extends PersistenciaJpa<QuestaoTemplate> implements QuestaoTemplateDao {
+public class QuestaoTemplateDaoJpa extends PersistenciaJpa<QuestaoTemplate>
+		implements QuestaoTemplateDao {
 
 	private static final long serialVersionUID = 1L;
-	
-	
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<QuestaoTemplate> consultarPor(String palavra, String tipo) {
+		try {
+			String jpaql = "select distinct qt from QuestaoTemplate qt "
+					+ " left join fetch qt.questao q ";
+			if (!palavra.isEmpty() || palavra != null) {
+				if (!tipo.isEmpty() || tipo != null) {
+					if (tipo == "Dificuldade") {
+						jpaql += " where upper(q.dificuldade) like :dificuldade";
+					}
+					if (tipo == "Assunto") {
+						jpaql += " where upper(q.assunto) like :assunto";
+					}
+					if (tipo == "Disciplina") {
+						jpaql += " where upper(q.disciplina) like :disciplina";
+					}
+				}
+			}
+			Query query = em.createQuery(jpaql);
+			if (!palavra.isEmpty() || palavra != null) {
+				if (tipo.isEmpty() || tipo == null) {
+					if (tipo == "Dificuldade") {
+						query.setParameter("dificuldade",
+								"%" + palavra.toUpperCase() + "%");
+					}
+					if (tipo == "Assunto") {
+						query.setParameter("assunto",
+								"%" + palavra.toUpperCase() + "%");
+					}
+					if (tipo == "Disciplina") {
+						query.setParameter("disciplina",
+								"%" + palavra.toUpperCase() + "%");
+					}
+				}
+			}
+			return (List<QuestaoTemplate>) query.getResultList();
+		} catch (NoResultException nre) {
+			return new ArrayList<QuestaoTemplate>();
+		}
+	}
 
 }
