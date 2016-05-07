94b7c22f7a76c180ad68cd042ba5d246dd7b2b5d
gea.raphael <gea.raphael@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2016-05-07 03:05:32 +0000

    Adição da dependencia XStream para converção de um xml para um objeto. Alterado as classes clientes que fazem a consulta no web service para receber e converter o xml em objeto. A conversão anterior onde é utilizada o GSON estáva apresentando erro ao consultar o json por isso a alteração.

--- src/main/java/br/com/ambientinformatica/fatesg/sgep/persistencia/InstituicaoDaoService.java
+++ src/main/java/br/com/ambientinformatica/fatesg/sgep/persistencia/InstituicaoDaoService.java
@@ -3,25 +3,33 @@ package br.com.ambientinformatica.fatesg.sgep.persistencia;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.RestTemplate;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.ambientinformatica.fatesg.api.entidade.Instituicao;
 import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
 
 @Repository("instituicaoDao")
-public class InstituicaoDaoService extends PersistenciaJpa<Instituicao> implements InstituicaoDao, Serializable {
+public class InstituicaoDaoService extends PersistenciaJpa<Instituicao>
+		implements InstituicaoDao, Serializable {
+	
+	private Client client = ClientBuilder.newClient();
 
-	private RestTemplate restTemplate = new RestTemplate();
+	private WebTarget target = client.target("http://localhost:8080/corporatum/service/instituicao");
+
 	
 	private static final long serialVersionUID = 1L;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Instituicao> listarPorNome(String nome) {
-		return restTemplate.getForObject("http://localhost:8180/corporatum/service/instituicao/listarPorNome/" + nome, List.class);
+		String conteudo = target.path("/listarPorNome/" + nome).request().get(String.class);
+		return (List<Instituicao>) new XStream().fromXML(conteudo);
 	}
 
-	
-	
 }
