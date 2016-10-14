e0f9fb2a963c3be13dc76c58ee45938bc8643980
eduardo.souza <eduardo.souza@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2016-10-14 22:31:06 +0000

    Refatoração - tela questão

---
+++ src/main/java/br/com/ambientinformatica/fatesg/sgep/entidade/EnumAlternativa.java
@@ -0,0 +1,19 @@
+package br.com.ambientinformatica.fatesg.sgep.entidade;
+
+import br.com.ambientinformatica.util.IEnum;
+
+public enum EnumAlternativa implements IEnum{
+	A("A"), B("B"), C("C"), D("D"), E("E");
+	
+	private final String descricao;
+	
+	private EnumAlternativa(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+}
